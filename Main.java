88e4811545b9af77a2948239eccd470f0e8c3244
Your Name <95087647+Wishwaka@users.noreply.github.com>
2024-11-28 13:32:06 +0530

    Implementation started2

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main {
         Vendor vendor1 = new Vendor();
         vendor1.setName("Wishwaka Livera");
         System.out.println(vendor1.getName());
+        vendor1.setName("Angelo Livera");
+        System.out.println(vendor1.getName());
     }
 }
